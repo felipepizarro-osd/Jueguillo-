e09c66fefdcda1c19b46deb0531f2cb865f68bc8
C19Software <68974992+C19Software@users.noreply.github.com>
2020-08-11 20:54:03 -0400

    nivel 4
    importación de maps nivel 4

---
+++ Juego PlataformaOriginal/Assets/SuperTiled2Unity/mapas/e4-JohnSnow/ice.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="ice" tilewidth="21" tileheight="21" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="21" height="21" source="ice.png"/>
+ </tile>
+</tileset>
