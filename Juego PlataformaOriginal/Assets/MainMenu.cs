b476e28db5b5b874976305421bea33c1b03f4abc
space-dog0 <left4dead123xd@hotmail.com>
2020-08-19 19:44:25 -0400

    Implementacion Menu (falta musica y menu opciones funcional)

---
+++ Juego PlataformaOriginal/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("nivel1.0", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+}
