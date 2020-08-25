96d69e5ee03c6783cc53eeadf3ff2b4250f6506f
CriScarFace <cristian.becerra@alumnos.ucn.cl>
2020-08-25 14:12:09 -0400

    sprites y script Jefe Final

--- Juego PlataformaOriginal/Assets/Scripts/EnemyIA.cs
+++ Juego PlataformaOriginal/Assets/Scripts/EnemyIA.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class EnemyIA : MonoBehaviour
 {
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    public float enemySpeed;
+
+    public bool goRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!goRight)
+        {
+            transform.position = startPoint.transform.position;
+        }
+        else
+        {
+            transform.position = endPoint.transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (goRight == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, enemySpeed * Time.deltaTime);
+            if (transform.position == endPoint.transform.position)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                goRight = false;
+            }
+        }
+
+        if (!goRight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, enemySpeed * Time.deltaTime);
+            if (transform.position == startPoint.transform.position)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                goRight = true;
+            }
+        }
     }
 }
