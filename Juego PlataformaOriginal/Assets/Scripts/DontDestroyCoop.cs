886e6efd96bbefc29bc5812dd442d36a6f556dd3
space-dog0 <left4dead123xd@hotmail.com>
2020-08-25 15:14:58 -0400

    Implementado coop en menu

---
+++ Juego PlataformaOriginal/Assets/Scripts/DontDestroyCoop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyCoop : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("player2active");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
