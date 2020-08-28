783b12b967645b32bf6490b6716e4e302aa152c7
space-dog0 <left4dead123xd@hotmail.com>
2020-08-27 21:01:33 -0400

    Agregado menu nuevo y menu de pausa, fix tutorial

---
+++ Juego PlataformaOriginal/Assets/Scripts/DestroyCoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var coop = GameObject.FindWithTag("player2active");
+        if(coop != null)
+            Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
