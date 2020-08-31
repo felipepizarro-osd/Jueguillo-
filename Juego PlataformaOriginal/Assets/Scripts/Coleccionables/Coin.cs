58f39f5ee63dddb0732ef05898a3490a2d446232
c4rloncho <68973311+c4rloncho@users.noreply.github.com>
2020-08-30 20:49:42 -0400

    Update Coin.cs
    sonido coin

--- Juego PlataformaOriginal/Assets/Scripts/Coleccionables/Coin.cs
+++ Juego PlataformaOriginal/Assets/Scripts/Coleccionables/Coin.cs
@@ -3,6 +3,7 @@
 public class Coin : MonoBehaviour
 {
     public int coinValue = 1;
+    public GameObject Soundcoin;
 
 
     // Start is called before the first frame update
@@ -10,11 +11,15 @@ public class Coin : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player") )
         {
+            
             ScoreManager.instance.ChangeScore(coinValue);
+            Instantiate(Soundcoin);
         }
         if (other.gameObject.CompareTag("player2"))
         {
+        
             ScoreManager2.instance2.ChangeScore2(coinValue);
+            Instantiate(Soundcoin);
         }
     }
 }
